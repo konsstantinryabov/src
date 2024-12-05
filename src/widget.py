dbf3ae58ea0465a2e8a4b2075fc6c434525a3fce
konstantin <konsstantinryabov@gmail.com>
2024-12-05 23:49:45 +0400

    создание файла widget.py добавление функции mask_account_card()

---
+++ src/widget.py
@@ -0,0 +1,16 @@
+from typing import Union
+
+from src import masks
+
+
+def mask_account_card(card_number: Union[str]) -> Union[str]:
+    """
+    Функция возвращает содержащую тип и номер карты или счет
+    получает: "Visa Platinum 7000792289606361"
+    возвращает: "Visa Platinum 7000 79** **** 6361"
+    """
+    data_card = card_number.split()
+    if data_card[0] == "Счет":
+        return f"{data_card[0]} {masks.get_mask_account(int(data_card[-1]))}"
+    else:
+        return f'{" ".join(data_card[:-1])} {masks.get_mask_card_number(int(data_card[-1]))}'
