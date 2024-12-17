209a5e81f8f79cc315b99cb2ed6530be6dcdbccd
konstantin <konsstantinryabov@gmail.com>
2024-12-17 17:59:16 +0400

    fix processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,4 @@
-from typing import Union
-
-
-def filter_by_state(word_list: Union[list], state: Union[str] = "EXECUTED") -> Union[list]:
+def filter_by_state(word_list: list, state: str = "EXECUTED") -> list:
     """
     Функция возвращает новый список словарей, содержащий
     только те словари, у которых ключ state соответствует указанному значению.
@@ -11,16 +8,16 @@ def filter_by_state(word_list: Union[list], state: Union[str] = "EXECUTED") -> U
     return [word_list[word] for word in range(len(word_list)) if state == word_list[word]["state"]]
 
 
-def sort_by_date(word_list: Union[list], sorting: Union[bool] = True) -> Union[list]:
+def sort_by_date(word_list: list, sorting: bool = True) -> list:
     """
     Функция сортировки данных по дате
     Получает: [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, ит.д.]
     Возвращает: отсортированный по дате список словарей
     """
-    data_sort = sorted([word_list[word]["date"] for word in range(len(word_list))], reverse=sorting)
+    data_sort = sorted([word_list[item]["date"] for item in range(len(word_list))], reverse=sorting)
     sort_list = []
     for data in data_sort:
-        for word in word_list:
-            if data in word["date"]:
-                sort_list.append(word)
+        for entry in word_list:
+            if data in entry["date"]:
+                sort_list.append(entry)
     return sort_list
