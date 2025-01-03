269c44334e9af1ce4561cb12fbeca810e43dbe81
konstantin <konsstantinryabov@gmail.com>
2025-01-04 01:03:28 +0400

    редактирование masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,14 +1,48 @@
 from typing import Union
 
-
 def get_mask_card_number(card_number: Union[int]) -> Union[str]:
     """Функция принимает на вход номер карты и возвращает ее маску"""
 
-    mask = f"{str(card_number)[:6]}******{str(card_number)[-4:]}"
-    return " ".join([mask[i : i + 4] for i in range(0, len(mask), 4)])
+    string_number_cart = str(card_number)
+    mask_number_card = ""
+
+    if card_number is None or not card_number:
+        mask_number_card = "0"
+    elif 12 <= len(str(card_number)) <= 20:
+        string_num_card_mask = ""
+        verification_numbers = "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
+        for letter in string_number_cart:
+            if letter in verification_numbers:
+                string_num_card_mask += letter
+            else:
+                continue
+        if len(str(string_num_card_mask)) == 16:
+            mask_number_card = (
+                f"{string_num_card_mask[0:4]} {string_num_card_mask[4:6]}** **** {string_num_card_mask[-4:]}"
+            )
+
+        elif len(str(string_num_card_mask)) == 12:
+            mask_number_card = f"{string_num_card_mask[0:2]}** **** {string_num_card_mask[-4:]}"
+    else:
+        mask_number_card = "Некорректный номер карты"
+    return mask_number_card
 
 
-def get_mask_account(account: Union[int]) -> Union[str]:
+def get_mask_account(card_number: Union[int]) -> Union[str]:
     """Функция принимает на вход номер счета и возвращает его маску"""
 
-    return f"**{str(account)[-4:]}"
+    string_number_cart = str(card_number)
+    if card_number is None or not card_number:
+        return "0"
+    elif 16 <= len(str(card_number)) <= 24:
+        string_number_cart_account = ""
+        number_review = "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
+        for letter in string_number_cart:
+            if letter not in number_review:
+                continue
+            else:
+                string_number_cart_account += letter
+
+        return f"**{string_number_cart_account[-4:]}"
+    else:
+        return "Некорректный номер счета"
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -4,8 +4,7 @@ from src.masks import get_mask_account, get_mask_card_number
 
 import pytest
 
-
-def test_card_review(card_review: str) -> None:
+def test_card_review(card_review: [str, int]) -> None:
     for card_number in card_review:
         if len(str(card_number)) == 16:
             assert get_mask_card_number(card_number) == "7000 79** **** 6361"
@@ -13,13 +12,12 @@ def test_card_review(card_review: str) -> None:
             assert get_mask_account(card_number) == "**4305"
 
 
-
 @pytest.mark.parametrize("card_number, expected", [
     ("7000792289606361", "7000 79** **** 6361"),
     ("7000 7922 8960 6361", "7000 79** **** 6361"),
     ("5995 1248 3579 2541", "5995 12** **** 2541"),
     ("5995 1248 3579", "59** **** 3579"),
-    ("15489653215487984461464", "Введен некорректный номер карты"),
+    ("15489653215487984461464", "Некорректный номер карты"),
     (5995124835792541, "5995 12** **** 2541"),
     ([], "0"),
     ({}, "0"),
@@ -34,7 +32,7 @@ def test_get_mask_card_number(card_number: Union[int, str], expected: str) -> No
     ("7365 4108 4301 3587 4305", "**4305"),
     ("5995 1248 3579 2541 7896", "**7896"),
     ("7365410843013587430", "**7430"),
-    ("15489653215487984461464", "Введен некорректный номер карты"),
+    ("15489653215487984461401464", "Некорректный номер счета"),
     (73654108430135874305, "**4305"),
     ([], "0"),
     ({}, "0"),
